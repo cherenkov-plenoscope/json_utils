73f94413444c5eb1c6e94998dd47ae81540862a6
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-05-08 16:05:30 +0200

    Make lines.Reader and lines.Writer accept filehandles, not just paths.

--- json_utils/lines.py
+++ json_utils/lines.py
@@ -3,84 +3,6 @@ import builtins
 import gzip
 
 
-def write(path, obj_list, mode="w"):
-    with Writer(path, mode) as jlwr:
-        for obj in obj_list:
-            jlwr.write(obj)
-
-
-def read(path, mode="r"):
-    obj_list = []
-    with Reader(path, mode) as jlre:
-        for obj in jlre:
-            obj_list.append(obj)
-    return obj_list
-
-
-class Reader:
-    def __init__(self, path, mode="r"):
-        assert "r" in mode
-        assert not "w" in mode
-        self.path = path
-        if str.endswith(mode, "|gz"):
-            self.filehandle = gzip.open(self.path, mode="rt")
-        else:
-            self.filehandle = builtins.open(self.path, mode="rt")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
-
-    def close(self):
-        self.filehandle.close()
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        line = self.filehandle.__next__().strip()
-        return json_numpy.loads(line)
-
-    def __repr__(self):
-        out = "{}(".format(self.__class__.__name__)
-        out += "path '" + self.path + "'"
-        out += ")\n"
-        return out
-
-
-class Writer:
-    def __init__(self, path, mode="w"):
-        assert not "r" in mode
-        assert "w" in mode
-        self.path = path
-        if str.endswith(mode, "|gz"):
-            self.filehandle = gzip.open(self.path, mode="wt")
-        else:
-            self.filehandle = builtins.open(self.path, mode="wt")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
-
-    def close(self):
-        self.filehandle.close()
-
-    def write(self, obj):
-        line = json_numpy.dumps(obj, indent=None)
-        self.filehandle.write(line)
-        self.filehandle.write("\n")
-
-    def __repr__(self):
-        out = "{}(".format(self.__class__.__name__)
-        out += "path '" + self.path + "'"
-        out += ")\n"
-        return out
-
-
 def open(path, mode="r"):
     """
     Read or write json lines.
@@ -98,9 +20,80 @@ def open(path, mode="r"):
     reader/writer : Reader/Writer
         Depending on mode.
     """
+    assert not "b" in mode, "Expected text-mode 't' and not binary 'b'."
     if "r" in mode:
-        return Reader(path=path, mode=mode)
+        if "|gz" in mode:
+            return Reader(file=gzip.open(path, "rt"))
+        else:
+            return Reader(file=builtins.open(path, "rt"))
     elif "w" in mode:
-        return Writer(path=path, mode=mode)
+        if "|gz" in mode:
+            return Writer(file=gzip.open(path, "wt"))
+        else:
+            return Writer(file=builtins.open(path, "wt"))
     else:
-        raise ValueError("mode must either contain 'r' or 'w'.")
+        raise ValueError("Expected mode to contain either 'w' or 'r'.")
+
+
+def write(path, obj_list, mode="w"):
+    with open(path=path, mode=mode) as jlwr:
+        for obj in obj_list:
+            jlwr.write(obj)
+
+
+def read(path, mode="r"):
+    obj_list = []
+    with open(path=path, mode=mode) as jlre:
+        for obj in jlre:
+            obj_list.append(obj)
+    return obj_list
+
+
+class Reader:
+    def __init__(self, file):
+        self.file = file
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def close(self):
+        self.file.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        line = self.file.__next__().strip()
+        return json_numpy.loads(line)
+
+    def __repr__(self):
+        out = "{}(".format(self.__class__.__name__)
+        out += ")\n"
+        return out
+
+
+class Writer:
+    def __init__(self, file):
+        self.file = file
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def close(self):
+        self.file.close()
+
+    def write(self, obj):
+        line = json_numpy.dumps(obj, indent=None)
+        self.file.write(line)
+        self.file.write("\n")
+
+    def __repr__(self):
+        out = "{}(".format(self.__class__.__name__)
+        out += ")\n"
+        return out
